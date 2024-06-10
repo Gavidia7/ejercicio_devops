728821f7390b8d340cf2afc24d1ec3455b3e6d62
Gavidia7 <gavidiacobo@gmail.com>
2024-06-10 22:49:04 +0200

    Pushing to Heroku

--- app.test.js
+++ app.test.js
@@ -5,7 +5,7 @@ const request = supertest(app);
 describe("/test endpoint", () => {
   test("Debe devolver un 'Hola Mundo'", async () => {
     const response = await request.get("/");
-    const body = response.body; 
+    const body = JSON.parse(response.text); 
     expect(response.status).toBe(200);
     expect(body.message).toBe("¡Hola mundo!");
   });
